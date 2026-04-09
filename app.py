fce6f554febcf1d87abab62a64d53e4e6a6e5858
Faris Wafda <111740313+Wafxd@users.noreply.github.com>
2026-04-09 17:49:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 app.secret_key = os.environ.get("SECRET_KEY", "rahasia")
 SUPABASE_URL = "sb_publishable_9TNIaYZW8DZpOr18lxKUIQ_ZK4YR3BJ" # Disesuaikan dengan URL mu
-SUPABASE_KEY = "sb_secret_8g2LjvxRsu29rRmsss4D-w_SeZSdqcD"
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
 
 try:
     supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
